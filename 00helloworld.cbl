20d19d70a4026e6ef0f0c66256477c2c8f462058
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2022-12-31 15:27:04 +0530

    Create 00helloworld.cbl

---
+++ 00helloworld.cbl
@@ -0,0 +1,22 @@
+****************************************************************** 
+* Author: Daniel Chakraborty 
+* Date: July 24 2021 
+* Purpose: Print Hello World 
+* Tectonics: cobc 
+****************************************************************** 
+IDENTIFICATION DIVISION. 
+PROGRAM-ID. HELLO-PROGRAM. 
+DATA DIVISION. 
+FILE SECTION. 
+WORKING-STORAGE SECTION. 
+01 name PIC X(19) VALUE " Daniel Chakraborty". 
+01 hello PIC X(6) VALUE "hello ". 
+01 finalString PIC X(25). 
+PROCEDURE DIVISION. 
+MAIN-PROCEDURE. 
+STRING hello DELIMITED BY SPACE 
+name DELIMITED BY SIZE 
+INTO finalString. 
+DISPLAY finalString. 
+STOP RUN. 
+END PROGRAM HELLO-PROGRAM. 
