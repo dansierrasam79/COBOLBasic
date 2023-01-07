e1488e4462ccf3c672b332da622d5b69810565f0
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2023-01-07 16:15:17 +0530

    Create 06multiplicationtable.cbl

---
+++ 06multiplicationtable.cbl
@@ -0,0 +1,29 @@
+*> Write a COBOL program that takes a number as input and prints its multiplication table up to 10. 
+IDENTIFICATION DIVISION. 
+PROGRAM-ID. DISPLAY-TIMES-TABLES. 
+DATA DIVISION. 
+WORKING-STORAGE SECTION. 
+	01 tables PIC 99. 
+	01 tableWord PIC X(2). 
+	01 intoSign PIC X(2) VALUE "x". 
+	01 countVal PIC 99 VALUE 01. 
+	01 countValWord PIC X(2).
+	01 equalTo PIC X(1) VALUE "=". 
+	01 result PIC 99. 
+	01 resultWord PIC X(2). 
+	01 destWord PIC X(10). 
+PROCEDURE DIVISION. 
+TIMESTABLESCOMPUTATION. 
+	DISPLAY "Enter a number: " 
+	ACCEPT tables 
+	MOVE tables TO tableWord 
+	PERFORM 10 TIMES 
+	MOVE countVal TO countValWord 
+	COMPUTE result = countVal * tables 
+	MOVE result TO resultWord 
+	STRING tableWord, intoSign, countValWord, equalTo, resultWord DELIMITED BY SIZE INTO destWord 
+	END-STRING 
+	DISPLAY destWord
+	COMPUTE countVal = countVal + 1 
+	END-PERFORM. 
+	STOP RUN. 
